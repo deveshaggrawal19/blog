283be94e653b9232a10dfd04101f318a0e3583a7
deveshaggrawal <deveshaggrawal19@gmail.com>
2016-09-16 22:12:58 +0530

    Final links to different posts

--- blogging/views.py
+++ blogging/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 # Create your views here.
@@ -8,3 +8,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())\
         .order_by('published_date')
     return render(request, 'blogging/post_list.html', {'posts': posts})
+
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blogging/post_details.html', {'post': post})
